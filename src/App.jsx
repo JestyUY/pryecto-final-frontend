6d979cd2c8d683a3c24248286f0827015a459464
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-08-11 02:26:03 +1200

    main page background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="w-6 h-12 bg-slate-500"></div>;
+  return (
+    <div className="bg-cover bg-no-repeat w-screen h-screen sm:max-w-3xl sm:max-h-[1200px] m-auto bg-[url('https://s3-alpha-sig.figma.com/img/aa2f/36c0/a746f7d5f282311ee9a5d9aba5746e99?Expires=1692576000&Signature=nA7E5jX4BYugZSz-pV2IXUo2gGqYqG-OIFqCzG09fapjw~4WZYG-e~bUTqLZyUip4QNZrOauH9pwpA1gT9Ki7iWMbZ8tRmU0AKAeqhf2D1NFAqZK2TGqhby18AFIQ~6u5DIszDnwZ2KKgjiSREdM7Z-d4gS87qhluuSIQH7aNJCNRxwjOuNQvZ3jmhAvFwlsXeoK~-OmmG70fsuviXU3WQk-heXllfclg0S899WSbm4l7IxmZZ61h-ibS1pc-Zk1Ipi8Wc2eESXyNAb2KjjJEqApQ7T~7r~2ip~btqVhxgU6awmcw6EVl4Q52D1QqrGxzvHYtSewI39AKRWIoaTllw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')]">
+      <div className="bg-gradient-to-t from-white from-10% to-transparent h-screen w-screen z-10"></div>
+    </div>
+  );
 }
 
 export default App;
+
+//  postgres://JestyUY:@ep-summer-breeze-77183421.us-east-2.aws.neon.tech/neondb
